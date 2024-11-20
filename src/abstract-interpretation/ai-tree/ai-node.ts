8cd5b90a20d4353a7cbbd3b0e4fbf7238847f4bb
manueldiagostino <diagostinomanuel@gmail.com>
2024-11-20 16:58:01 +0100

    refactor(absint): fixed path in `ai-node.ts`

--- src/abstract-interpretation/ai-tree/ai-node.ts
+++ src/abstract-interpretation/ai-tree/ai-node.ts
@@ -1,5 +1,5 @@
-import type { NoInfo } from '../r-bridge/lang-4.x/ast/model/model';
-import type { Visitor } from './normalized-ast-visitor';
+import type { NoInfo } from '../../r-bridge/lang-4.x/ast/model/model';
+import type { Visitor } from '../normalized-ast-visitor';
 
 export interface AINode<Info = NoInfo> {
 	accept(visitor: Visitor<Info>): void;
